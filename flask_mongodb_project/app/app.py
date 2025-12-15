04beb08edb761bd0ee9d0f57f8366da210e44415
Nitish Kumar <kumarnitish27212@gmail.com>
2025-12-16 00:27:27 +0530

    render

--- flask_mongodb_project/app/app.py
+++ flask_mongodb_project/app/app.py
@@ -3,16 +3,22 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 import re
 from bson.objectid import ObjectId
+import os
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Create Flask app
 app = Flask(__name__)
+application = app   # for deployment (Render / Gunicorn)
 
 # ---- MongoDB Connection ----
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(os.getenv("MONGO_URI"))
 db = client["peopleXM"]
 users_collection = db["user"]
 
-#--------------------- Validation Schema ---------------
-# all fields must be STRING
+# ------------------ Validation Schema ------------------
 user_schema = {
     "first_name": str,
     "last_name": str,
@@ -20,46 +26,38 @@ user_schema = {
 }
 
 def validate_user(data):
-
     for key in user_schema.keys():
+
         if key not in data:
             return False, f"{key} is missing"
 
         value = data[key]
 
-        # must be string
         if type(value) is not str:
             return False, f"{key} must be a string only"
 
-        # empty not allowed
         if value.strip() == "":
             return False, f"{key} cannot be empty"
 
-        # only alphabets for names
         if key in ['first_name', 'last_name']:
             if not re.match(r'^[A-Za-z]+$', value):
                 return False, f"{key} must contain only alphabets"
 
     return True, "Valid"
 
+# ------------------ Routes ------------------
 
-
-
-#----------------------------------all routes----------------------------------------------------
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#----------------------------------all data showing----------------------------------------------
 @app.route('/show')
 def show_users():
     users = list(users_collection.find())
     return render_template("show.html", users=users)
 
-#-----------------------------------new user register---------------------------------------------
 @app.route('/user', methods=['POST'])
 def add_user():
-
     data = request.form
 
     user = {
@@ -75,22 +73,13 @@ def add_user():
     users_collection.insert_one(user)
     return redirect(url_for('show_users'))
 
-
-#---------------------------------------get users from database------------------------------------
-
 @app.route("/users", methods=["GET"])
 def get_users():
     users = list(users_collection.find({}, {"_id": 0}))
     return jsonify(users)
 
-#--------------------------------------Update user details----------------------------------------
-# @app.route('/user/<user_id>')
-# def user_by_id():
-
-
 @app.route('/user/<user_id>', methods=['GET', 'POST'])
 def update_user(user_id):
-
     user = users_collection.find_one({'_id': ObjectId(user_id)})
 
     if request.method == 'POST':
@@ -113,17 +102,18 @@ def update_user(user_id):
 
     return render_template('update_user.html', user=user)
 
-
-
-#--------------------------------------Delete user details----------------------------------------
 @app.route('/delete/<user_id>', methods=['POST'])
 def delete_user(user_id):
     users_collection.delete_one({'_id': ObjectId(user_id)})
     return redirect(url_for('show_users'))
 
-
-#--------------------------------------Update user details----------------------------------------
-
-#---------------------------------------server live at 8000----------------------------------------
+# ------------------ Run Server ------------------
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+
+
+
+
+
+
+#mongodb+srv://kumarnitish27212_db_user:NvRLBnXYtd8kAA5V@cluster0.efvfnzn.mongodb.net/?appName=Cluster0
